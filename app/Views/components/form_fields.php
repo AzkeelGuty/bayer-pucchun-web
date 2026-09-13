8631afab1fec533652b12d7d2d9f524d761bf37a
AzkeelGuty <150597935+AzkeelGuty@users.noreply.github.com>
2026-09-13 14:53:24 -0500

    fix(forms): mejora campos responsive y decimales

--- app/Views/components/form_fields.php
+++ app/Views/components/form_fields.php
@@ -1,1 +1,14 @@
-<?php function field($name,$label,$type='text',$required=true){$old=$_SESSION['_old'][$name]??'';$err=$_SESSION['_errors'][$name]??'';echo '<div class="col-md-4"><label class="form-label">'.e($label).'</label><input class="form-control '.($err?'is-invalid':'').'" type="'.e($type).'" name="'.e($name).'" value="'.e($old).'" '.($required?'required':'').'><div class="invalid-feedback">'.e($err).'</div></div>';} ?>
+<?php
+function field($name,$label,$type='text',$required=true)
+{
+    $old=$_SESSION['_old'][$name]??'';
+    $err=$_SESSION['_errors'][$name]??'';
+    $step=$type==='number' ? ' step="any"' : '';
+    $req=$required ? ' required' : '';
+    echo '<div class="col-12 col-md-6 col-xl-4">';
+    echo '<label class="form-label" for="'.e($name).'">'.e($label).'</label>';
+    echo '<input id="'.e($name).'" class="form-control '.($err?'is-invalid':'').'" type="'.e($type).'" name="'.e($name).'" value="'.e($old).'"'.$step.$req.'>';
+    if($err) echo '<div class="invalid-feedback">'.e($err).'</div>';
+    echo '</div>';
+}
+?>
